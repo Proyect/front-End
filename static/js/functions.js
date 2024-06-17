4ee6f9c477e73d71083c3cfd244f3db8b66b304d
infrasoft <ariel@infrasoft.com.ar>
2024-06-17 15:45:32 -0300

    .

--- static/js/functions.js
+++ static/js/functions.js
@@ -1,22 +1,2 @@
-  //Variable global para guardar el intervalo
-  var animInterval;
 
-  function startAnim() {
-
-    let obj = document.getElementById('valor-cesta');
-    animInterval = setInterval(animNumValue, 100, obj, 3000);
-  }
-
-  function animNumValue(num, newValue) {
-
-    valor = num.innerHTML;
-    intValue = parseInt(valor);
-
-    if (intValue >= newValue) { //Si el valor ha superado el objetivo paro
-      clearInterval(animInterval);
-    } else { //Si el valor no ha superado el objetivo lo aumento
-      valor++;
-      num.innerHTML = valor;
-    }
-  }
-  startAnim();
+ // Code here
